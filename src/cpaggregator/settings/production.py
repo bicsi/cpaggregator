b31805d0d9f9dd5cc0539eab3ecfad371b7e14e6
Lucian Bicsi <lucianbicsi@gmail.com>
2018-10-19 13:23:33 +0300

    Fixed populate db?

--- src/cpaggregator/settings/production.py
+++ src/cpaggregator/settings/production.py
@@ -16,9 +16,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
-        os.path.dirname(
             os.path.dirname(
-                os.path.abspath(__file__))))
+                os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9n03926ncnffiw(6np41+zv9pv@q=vda4o_sgit&**(u*9ckr1'
