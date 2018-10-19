bd18f7f38a405ce07ca42cb8f26072254966512c
Lucian Bicsi <lucianbicsi@gmail.com>
2018-10-19 12:07:13 +0300

    Try again

--- src/cpaggregator/settings/local.py
+++ src/cpaggregator/settings/local.py
@@ -1,6 +1,6 @@
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
         os.path.dirname(
             os.path.dirname(
