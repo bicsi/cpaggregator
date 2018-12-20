5a2125b1cc658739fa5884488db85645749dd12a
Lucian Bicsi <lucianbicsi@gmail.com>
2018-12-20 18:13:40 +0200

    Please try again

--- src/cpaggregator/settings/production.py
+++ src/cpaggregator/settings/production.py
@@ -138,6 +138,8 @@ django_heroku.settings(locals(), staticfiles=False)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'assets')]
+# Ensure STATIC_ROOT exists.
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
