d4c9982e7b83c5103c5a2214014d48a18df5789a
Lucian Bicsi <lucianbicsi@gmail.com>
2018-12-20 17:17:55 +0200

    Added logging?

--- src/cpaggregator/settings/production.py
+++ src/cpaggregator/settings/production.py
@@ -119,6 +119,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
