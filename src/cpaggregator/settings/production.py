884b54b57cfca108249ebf641bf7d447e781620e
Lucian Bicsi <lucianbicsi@gmail.com>
2018-12-20 18:05:11 +0200

    Please static work

--- src/cpaggregator/settings/production.py
+++ src/cpaggregator/settings/production.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cpaggregator.urls'
@@ -136,6 +137,7 @@ django_heroku.settings(locals(), staticfiles=False)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'assets')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
