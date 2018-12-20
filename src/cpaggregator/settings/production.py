4f8793047a75d99c9c67211b7f056f06eea8ff7e
Lucian Bicsi <lucianbicsi@gmail.com>
2018-12-20 17:57:48 +0200

    PWP

--- src/cpaggregator/settings/production.py
+++ src/cpaggregator/settings/production.py
@@ -134,6 +134,7 @@ django_heroku.settings(locals(), staticfiles=False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'assets')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
