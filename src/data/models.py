3c351590fbc6a7152e1f835fff105bf1faa81bc6
Lucian Bicsi <lucianbicsi@gmail.com>
2019-02-04 01:20:51 +0200

    Modified a link

--- src/data/models.py
+++ src/data/models.py
@@ -153,7 +153,10 @@ class Task(models.Model):
             return 'https://www.infoarena.ro/problema/%s' % self.task_id
         if self.judge.judge_id == 'cf':
             contest_id, task_letter = self.task_id.split('_')
-            return 'https://codeforces.com/contest/%s/problem/%s' % (contest_id, task_letter)
+            if contest_id >= 100000:
+                # Gym contest ids are always >= 100000
+                return f'https://codeforces.com/gym/{contest_id}/problem/{task_letter.upper()}'
+            return f'https://codeforces.com/contest/{contest_id}/problem/{task_letter.upper()}'
         return None
 
     class Meta:
