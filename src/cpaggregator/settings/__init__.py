e32e124d195ab95d82d9c5e63eefe7d368340297
Lucian Bicsi <lucianbicsi@gmail.com>
2019-10-28 22:14:42 +0200

    Changed user and task statistics to be computed at a normal rate

--- src/cpaggregator/settings/__init__.py
+++ src/cpaggregator/settings/__init__.py
@@ -61,11 +61,11 @@ CELERY_BEAT_SCHEDULE = {
     },
     # Stats services cronjobs.
     'compute-user-statistics': {
-        'schedule': 30,
+        'schedule': 30 * 60,
         'task': 'stats.services.compute_user_statistics',
     },
     'compute-task-statistics': {
-        'schedule': 5 * 60,
+        'schedule': 30 * 60,
         'task': 'stats.services.compute_task_statistics',
     },
 }
