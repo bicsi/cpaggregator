4eddb497adf7b8767e029e22e27894f7cd1f2d2e
Lucian Bicsi <bicsi@ymail.com>
2020-01-10 18:19:46 +0200

    Fixed chronjobs

--- src/cpaggregator/settings/__init__.py
+++ src/cpaggregator/settings/__init__.py
@@ -32,24 +32,24 @@ CELERY_BEAT_SCHEDULE = {
     'scrape-submissions-tasks-last-day': {
         'schedule': 5 * 60,
         'task': 'scraper.services.scrape_submissions_for_tasks',
-        'args': ('ac:*', 'ojuz:*', 'csa:*'),
+        'args': ('ac/*', 'ojuz/*', 'csa/*'),
         'kwargs': {'to_days': 1},
     },
     'scrape-submissions-tasks-all-time': {
         'schedule': 10 * 60 * 60,
         'task': 'scraper.services.scrape_submissions_for_tasks',
-        'args': ('ac:*', 'ojuz:*', 'csa:*'),
+        'args': ('ac/*', 'ojuz/*', 'csa/*'),
     },
     'scrape-submissions-users-last-day': {
         'schedule': 5 * 60,
         'task': 'scraper.services.scrape_submissions_for_users',
-        'args': ('cf:*', 'ia:*'),
+        'args': ('cf/*', 'ia/*'),
         'kwargs': {'to_days': 1},
     },
     'scrape-submissions-users-all-time': {
         'schedule': 10 * 60 * 60,
         'task': 'scraper.services.scrape_submissions_for_users',
-        'args': ('cf:*', 'ia:*'),
+        'args': ('cf/*', 'ia/*'),
     },
     #'scrape-tasks-info': {
     #    'schedule': 60 * 60,
