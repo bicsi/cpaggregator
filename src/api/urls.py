92603b7293efa3032f8949115355bc771ba6eee9
Lucian Bicsi <lucianbicsi@gmail.com>
2020-08-31 18:17:48 +0300

    Minifix

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('ladders/', include([
         path('rank/', views.ladders.ListLadderRank.as_view(), name='api-list-rank'),
     ])),
-    path('ladder/<user>/', include([
+    path('user/<user>/ladder/', include([
         path('', views.ladders.ShowLadder.as_view(), name='api-show-ladder'),
         path('subs/best/', views.ladders.ListBestSubmissions.as_view(), name='api-list-subs-ladder'),
     ])),
