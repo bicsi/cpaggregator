26bb8b056af8246ea14d3b8de95a4124b4378bf2
Lucian Bicsi <lucianbicsi@gmail.com>
2020-11-04 00:59:34 +0200

    Added expires on field to tokens

--- src/api/views/token.py
+++ src/api/views/token.py
@@ -16,6 +16,28 @@ class TokenObtainPairSerializer(jwt_serializers.TokenObtainPairSerializer):
         return data
 
 
+
+class TokenRefreshSerializer(jwt_serializers.TokenRefreshSerializer):
+    def get_token(self, user):
+        token = RefreshToken.for_user(user)
+        self.token = token 
+        return token
+
+    def validate(self, attrs):
+        token = RefreshToken(attrs['refresh'])
+
+        data = {
+            'access': str(token.access_token),
+            'expires_on': datetime.now() + token.lifetime,
+        }
+        return data
+
+
+
 class TokenObtainPairView(jwt_views.TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
 
+
+
+class TokenRefreshView(jwt_views.TokenRefreshView):
+    serializer_class = TokenRefreshSerializer
